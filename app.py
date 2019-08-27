70625e44c323d2eee6d49e303bd9bf7cb2342e12
Naruedol <51944537+Naruedol@users.noreply.github.com>
2019-08-27 14:23:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,11 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    user = decoded["events"][0]['replyToken']
-    userText = decoded["events"][0]['message']['text']
+    #user = decoded["events"][0]['replyToken']
+    #userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
+    user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
+    userText = decoded['queryResult']['intent']['displayName']
     if   (usertext == 'Hi') :
          sendtext(user,'Hello','What is yourname?')
     elif (usertext == 'Nine') :
